bef2da494ddc10475e3ae686a964d0b683558516
Jonah Kanner <jkanner@caltech.edu>
2023-04-03 16:06:05 -0700

    add instructions

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ import time
 st.write("# Group Paper List")
 
 
+st.write("## Instructions")
+
+st.write("""
+
+1. Check out the [LIGO Lab Group paper list](https://docs.google.com/spreadsheets/d/1zxE7Zlatfgl07ocoJe-1LpNOeFOKHjFjLxplOxGlKzI/edit?usp=sharing)
+
+2. Use the form below to query for your recent papers
+
+3. Download the CSV, and then copy and paste the data into the paper list
+
+""")
+
+
 with st.form("queryform"):
     
     authorf = st.text_input('Author First Name')
